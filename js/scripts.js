d48993096af2b77ca8885833d2a51370f0927fb7
Aaron Rohrbacher and Noah Gottlieb <aaron@>
2017-08-23 15:29:37 -0700

    create working game with clearing field

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,16 @@ Player.prototype.score = function(score){
   this.currentScore += score;
   this.turnScore = 0;
 }
-
+Player.prototype.winner = function(score){
+  var current = this.currentScore;
+  if(current + score >= 100){
+    player1.turnScore = 0;
+    player1.currentScore = 0;
+    player2.turnScore = 0;
+    player2.currentScore = 0;
+  alert(this.name + " wins! With a score of: " + (current + score));
+  }
+}
 
 player1 = new Player("Noah", [], 0, 0);
 player2 = new Player("Aaron", [], 0, 0);
@@ -30,38 +39,49 @@ player2 = new Player("Aaron", [], 0, 0);
 $(document).ready(function(){
   $(".rollOne").click(function(){
     var roll = player1.roll();
-    alert(roll);
-    alert(player1.turnScore)
-    alert(player1.currentScore);
+    var current = player1.currentScore;
+    player1.winner(roll);
     if(roll === 0){
+      $("#playerOneDisplayRoll").empty();
       $(".playerTwo").toggle();
       $(".playerOne").toggle();
-      alert(player1.turnScore);
-      alert(player1.currentScore);
+    } else {
+    $(".playerOneName").text(player1.name);
+    $("#playerOneDisplayRoll").append("<li>Your Roll: " + roll +  " | Current Score: " + (current + player1.turnScore) + "</li>");
     }
   })
   $(".holdOne").click(function() {
     player1.score(player1.turnScore);
     $(".playerOne").toggle();
     $(".playerTwo").toggle();
-    alert(player1.currentScore);
+    $("#playerOneCurrentScore").text(player1.currentScore);
+    $("#playerOneDisplayRoll").empty();
+
+
   })
   $(".rollTwo").click(function(){
     var roll = player2.roll();
-    alert(roll);
-    alert(player2.turnScore)
-    alert(player2.currentScore);
+    var current = player2.currentScore;
+    player2.winner(roll);
     if(roll === 0){
+      $("#playerTwoDisplayRoll").empty();
       $(".playerTwo").toggle();
       $(".playerOne").toggle();
-      alert(player2.turnScore);
-      alert(player2.currentScore);
+    } else {
+    $(".playerTwoName").text(player2.name);
+    $("#playerTwoDisplayRoll").append("<li>Your Roll: " + roll +  " | Current Score: " + (current + player2.turnScore) + "</li>");
     }
   })
   $(".holdTwo").click(function() {
     player2.score(player2.turnScore);
     $(".playerOne").toggle();
     $(".playerTwo").toggle();
-    alert(player2.currentScore);
+    $("#playerTwoCurrentScore").text(player2.currentScore);
+    $("#playerTwoDisplayRoll").empty();
+
+
   })
 })
+
+
+// $(".playerOne ul").append("<li>Your Roll: " + roll +  " = " + (current + player1.turnScore) + "</li>");
